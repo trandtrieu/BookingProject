e76db57e013117a68583f5998e9e62832c0f4fc9
trandtrieu <114125026+trandtrieu@users.noreply.github.com>
2023-05-30 07:28:17 +0700

    upadte jakarta

--- src/main/java/servlet/DetailServlet.java
+++ src/main/java/servlet/DetailServlet.java
@@ -7,10 +7,10 @@ package servlet;
 import dao.TourDao;
 import java.io.IOException;
 import java.util.List;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import model.Tour;
 
 /**
--- src/main/java/servlet/DetailTourGuide.java
+++ src/main/java/servlet/DetailTourGuide.java
@@ -8,10 +8,10 @@ import dao.GuideDao;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import model.GuideTour;
 import model.Tour;
 
